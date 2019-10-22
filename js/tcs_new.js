b97718857372ee87420f3657e04a782d0706b685
TonyChenSmith <a2898113823@163.com>
2019-10-22 22:36:14 +0800

    2019/10/22

--- js/tcs_new.js
+++ js/tcs_new.js
@@ -1,1 +1,39 @@
 //新的tcs.js
+var zh_CN_Header={"tcs_point":"跳转","tcs_lang":"转为：英语","tcs_guide_home":"主页","tcs_guide_about":"关于"};
+var en_US_Header={"tcs_point":"Turn","tcs_theme":"To:Chinese","tcs_guide_home":"Home","tcs_guide_about":"About"};
+
+var tcs_active={"page":"","themeType":""};
+
+//获得参数
+function TCSGetQueryString(name)
+{
+	var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r!=null)return unescape(r[2]); return null;
+}
+
+//亮色主题
+function TCSHeaderLightTheme(lang)
+{
+	$("body").css("background-color","#F1F1F1");
+	$("#tcs_header a").css("color","#F5F5F5");
+	$(".navbar").css({"background-color":"#0292ED","border":"none"});
+	$(".navbar-toggle").css("cssText","background-color:#32AEFD !important;border-color:#F5F5F5 !important");
+	$(".navbar-toggle").mousedown(function(){$(this).css("cssText","background-color:#016AAD !important");});
+	$(".navbar-toggle").mouseup(function(){$(this).css("cssText","background-color:#32AEFD !important");});
+	$(".navbar-toggle").hover(function(){$(this).css("cssText","background-color:#016AAD !important");},function(){$(this).css("cssText","background-color:#32AEFD !important");});
+	$(".icon-bar").css("cssText","background-color:#B2B2B2 !important");
+}
+
+//暗色主题
+function TCSHeaderDarkTheme(lang)
+{
+	$("body").css("background-color","#151515");
+	$("#tcs_header a").css("color","#6CC5FD");
+	$(".navbar").css({"background-color":"#003659","border":"none"});
+	$(".navbar-toggle").css("cssText","background-color:#016AAD !important;border-color:#6CC5FD !important");
+	$(".navbar-toggle").mousedown(function(){$(this).css("cssText","background-color:#32AEFD !important");});
+	$(".navbar-toggle").mouseup(function(){$(this).css("cssText","background-color:#016AAD !important");});
+	$(".navbar-toggle").hover(function(){$(this).css("cssText","background-color:#32AEFD !important");},function(){$(this).css("cssText","background-color:#016AAD !important");});
+	$(".icon-bar").css("cssText","background-color:#CECECE !important");
+}
